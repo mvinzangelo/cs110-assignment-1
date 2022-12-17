958a5ee89b7680d19334a27ddd24d94cbd4a7f2f
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-12-16 21:10:52 -0800

    Add centered image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import mainPicture from './photos/main.jpg'
 
 function App() {
   return (
     <div className="App">
-
+      <div className="InitialContainer">
+        <img className="MainPicture" src={mainPicture} alt="Man on a beach"></img>
+      </div>
     </div>
   );
 }
