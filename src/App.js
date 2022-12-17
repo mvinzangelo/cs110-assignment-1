2b2aff38c5d134ec4ea98bbe5c3ba618242f4b29
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-12-16 22:13:04 -0800

    Layout about portion

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ function App() {
     <div className="App">
       <div className="InitialContainer">
         <img className="MainPicture" src={mainPicture} alt="Man on a beach"></img>
-        <p className='AboutText'>
-          Vinz Angelo Madrigal is a Filipino Computer Science undergraduate based in Corona, CA currently studying at the University of California, Riverside pursuing a Bachelors in Computer Science. He is expected to graduate with a B.S in Computer Science in Fall of 2024.
-          <br /><br />
-          He is passionate about creating valuable experiences through software, whether by creating time saving applications or time sinking video games. He’s currently trying to find his place in the expansive world of technology by passionately trying to find what he’s passionate about.
-          <br /><br />
+        <div className="AboutInfoContainer">
+          <p className='AboutText'>
+            Vinz Angelo Madrigal is a Filipino Computer Science undergraduate based in Corona, CA currently studying at the University of California, Riverside pursuing a Bachelors in Computer Science. He is expected to graduate with a B.S in Computer Science in Fall of 2024.
+          </p>
+          <p className='AboutText'>
+            He is passionate about creating valuable experiences through software, whether by creating time saving applications or time sinking video games. He’s currently trying to find his place in the expansive world of technology by passionately trying to find what he’s passionate about.
+          </p>
           <ul className='Links'>
             <li>Email</li>
             <li>Resume</li>
@@ -18,8 +20,7 @@ function App() {
             <li>Instagram</li>
             <li>GitHub</li>
           </ul>
-
-        </p>
+        </div>
       </div>
     </div>
   );
